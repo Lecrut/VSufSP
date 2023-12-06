6c7c83a3196971f72dbfac45c38c7effdbe75ad7
Lecrut <filip.lecrut12@gmail.com>
2023-12-06 20:05:09 +0100

    opencv VideoCapture

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import cv2 as cv
+import numpy as np
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# todo: zmniejszyć zdjęcia o połowę minimum
+# todo: wyeliminować tło
+
+
+def watching_potatoes():
+    cap = cv.VideoCapture('images/ziemniak_dobry.jpg')
+    # cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    ret, frame = cap.read()
+
+    while True:
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    watching_potatoes()
